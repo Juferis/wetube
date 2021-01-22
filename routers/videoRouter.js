fc4465e872d7924094dd659bf9684c60f5111f25
Juferis <66511872+Juferis@users.noreply.github.com>
2021-01-22 15:33:34 +0900

    update user,video router

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,19 @@
 import express from "express";
+import routes from "../routes";
+import {
+  upload,
+  videoDelete,
+  videoDetail,
+  videoEdit,
+  videos,
+} from "../controllers/vidoeController";
 
 const videoRouter = express.Router();
+
+videoRouter.get("/", videos);
+videoRouter.get(routes.upload, upload);
+videoRouter.get(routes.videoDetail, videoDetail);
+videoRouter.get(routes.videoDetail, videoDelete);
+videoRouter.get(routes.videoEdit, videoEdit);
 
 export default videoRouter;
