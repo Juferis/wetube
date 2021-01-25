0bdce02f7a7785e071cb9a45a6317abbfd355bc5
Juferis <66511872+Juferis@users.noreply.github.com>
2021-01-25 15:08:33 +0900

    update searching
    pug and controller

--- controllers/vidoeController.js
+++ controllers/vidoeController.js
@@ -1,6 +1,13 @@
 export const home = (req, res) => res.render("home", { pageTitle: "Home" });
-export const search = (req, res) =>
-  res.render("search", { pageTitle: "Search" });
+export const search = (req, res) => {
+  // ES6 방식
+  const {
+    query: { term: searchingBy },
+  } = req;
+  // ES6 이전의 방식
+  // const searchingBy = req.query.term
+  res.render("search", { pageTitle: "Search", searchingBy });
+};
 export const videos = (req, res) =>
   res.render("videos", { pageTitle: "Videos" });
 export const upload = (req, res) =>
