764e5121b94bead678bc3822f29fde49e13c0e86
Juferis <66511872+Juferis@users.noreply.github.com>
2021-02-01 12:31:15 +0900

    install dotenv

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+//.env파일 안에 있는 정보를 가져온다
+dotenv.config();
 
 // mongodb://localhost:포트번호/DataBase이름 포트번호는 wsl에 mongod 치면 나옴
-mongoose.connect("mongodb://localhost:27017/we-tube", {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true, //새 버전엔 기본적으로 되어 있지만 해두는게 좋다
   useFindAndModify: false,
 });
--- init.js
+++ init.js
@@ -1,7 +1,11 @@
 import "./db";
 import app from "./app";
+import dotenv from "dotenv";
 
-const PORT = 4000;
+//.env파일 안에 있는 정보를 가져온다
+dotenv.config();
+
+const PORT = process.env.PORT || 4000; //.env 에서 포트를 가져오는데 실패하면 4000을 준다
 
 const handleListening = () =>
   console.log(`✅ Listening on: http://localhost:${PORT}`);
